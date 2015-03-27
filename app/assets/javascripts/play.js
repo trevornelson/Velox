1a8427c6e9e7c6dd3f7f6bc4cc578bc22862b448
Leo Rogers <leo.a.rogers@gmail.com>
2015-03-27 15:36:10 -0400

    hydrating hotel objects with api response data

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,67 +1,57 @@
-(function(){
+//Create Hotel Class
+var Hotel = function(data) {
+  this.name = data.attr.heading;
+  this.city = data.location.city;
+  this.neighbourhood = data.location.neighbourhood;
+  this.postalCode = data.location.postalCode;
+  this.streetName = data.location.streetName;
+  this.price = data.price.nightly;
+  this.provider = data.provider.full;
+  this.availability = data.availabity;
+  this.photo_url = data.photos[0].xlarge;
+  this.url = data.provider.url;
+};
+
+//hydrates hotel objects from API output
+var createHotels = function(data) {
+  var hotels = [];
+  for (i=0; i<data.length; i++) {
+    hotels.push(new Hotel(data[i]));
+  }
+  return hotels;
+};
+
+//create function to fetch hotels from API
+var fetchHotels = function() {
+  $.ajax({
+    url: 'https://zilyo.p.mashape.com/search?latitude=52.5306438&longitude=13.3830683&pricemax=200&provider=airbnb',
+    type: 'POST',
+    dataType: 'json',
+    success: function(data) {
+      hotels = createHotels(data.result);
+      console.log(hotels);
+    },
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-Mashape-Authorization", "N7SrCXP14imshrRVT7zdeMHz9NeLp1va6vFjsnpDJD7Fi1jnFg");
+    }
+  });
+}
+
+//enclosure containing hither module
+// (function(){
 
   var myAppModule = angular.module('hither', []);
 
-  myAppModule.controller('FlightController', function() {
-    this.flights = flights;
-    this.duration = function(flight) {
-      var dur = ((new Date(flight.arrive)) - (new Date(flight.depart)));
-      return dur;
-    };
+  //create hotel controller
+  myAppModule.controller('HotelController', function() {
+    this.hotels = [];
+
   });
 
-  var flights = [
-    {name: "flight1", price: "800", depart: "2015-03-24T06:57:23+00:00", arrive: "2015-03-24T09:57:23+00:00"},
-    {name: "flight2", price: "300", depart: "2015-03-24T04:57:23+00:00", arrive: "2015-03-24T10:57:23+00:00"},
-    {name: "flight3", price: "400", depart: "2015-03-24T06:57:23+00:00", arrive: "2015-03-24T06:57:23+00:00"}
-  ];
-
-})();
-
-thing = {
-  "request": {
-    "slice": [
-      {
-        "origin": "JAN",
-        "destination": "KTN",
-        "date": "2015-03-27"
-      }
-    ],
-    "passengers": {
-      "adultCount": 1,
-      "infantInLapCount": 0,
-      "infantInSeatCount": 0,
-      "childCount": 0,
-      "seniorCount": 0
-    },
-    "solutions": 5,
-    "refundable": false
-  }
-}
+// })();
 
 $(document).on("ready", function(){
 
-  // $.ajax({
-  //   type: 'POST',
-  //   url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyDskBz8WieP74_yU_hA7tVRvL1Ifl4ED1w',
-  //   contentType: 'application/json',
-  //   data: JSON.stringify(thing),
-  //   dataType: "json",
-  //   success: function(resp) {
-  //     // console.log(JSON.stringify(resp));
-  //   }
-  // });
-
-  // $.ajax({
-  //   url: 'https://zilyo.p.mashape.com/search?latitude=52.5306438&longitude=13.3830683&pricemax=200&provider=airbnb',
-  //   type: 'POST',
-  //   dataType: 'json',
-  //   success: function(data) {
-  //     // console.log(data)
-  //   },
-  //   beforeSend: function(xhr) {
-  //     xhr.setRequestHeader("X-Mashape-Authorization", "N7SrCXP14imshrRVT7zdeMHz9NeLp1va6vFjsnpDJD7Fi1jnFg");
-  //   }
-  // });
+  fetchHotels();
 
 })
