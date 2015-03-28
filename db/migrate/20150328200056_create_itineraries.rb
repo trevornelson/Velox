1e327568964ce8339ac44253fbc7a257eef0195d
Leo Rogers <leo.a.rogers@gmail.com>
2015-03-28 16:32:19 -0400

    fixed itinerary migration

--- db/migrate/20150328200056_create_itineraries.rb
+++ db/migrate/20150328200056_create_itineraries.rb
@@ -2,8 +2,6 @@ class CreateItineraries < ActiveRecord::Migration
   def change
     create_table :itineraries do |t|
       t.references :user
-      t.references :trip
-      t.references :hotel
 
       t.timestamps
     end
