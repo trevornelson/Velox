421f7a51122c826b4d3d8693630f5b9054aaf835
Christopher Selden <cbpselden@gmail.com>
2015-03-28 14:39:34 -0400

    added trip_spec test

---
+++ spec/javascripts/controllers/trip_spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('TripController', function() {
+
+  var $rootScope, $scope, $controller;
+
+  beforeEach(module('hither'));
+
+  beforeEach(inject(function(_$rootScope_, _$controller_){
+    $rootScope = _$rootScope_;
+    $scope = $rootScope.$new();
+    $controller = _$controller_;
+
+    $controller('TripController', {'$rootScope': $rootScope, '$scope': $scope});
+  }));
+
+  it('should return a trip that contains an array of flight(s)', function() {
+    expect($scope.trips.constructor.name === Array);
+  });
+
+})
