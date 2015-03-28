5b42f88fb2e18659a40ee5ab8fb8aaac918f7abd
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-28 19:36:08 -0400

    Created initial selectResult searchFactory function

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,15 +1,58 @@
 var myAppModule = angular.module('hither', []);
 
-myAppModule.controller('SearchController', function($scope) {
+angular.module('hither')
+  .controller('SearchController', ['$scope', 'searchFactory',
+    function($scope, searchFactory) {
+      var search = searchFactory.buildSearch({});
 
-});
+      $scope.keyup = function() {
+        searchFactory.query();
+      }
+  }
+]);
+
+angular.module('hither')
+  .factory('searchFactory', function() {
+
+    var factory = {};
+
+    factory.query = function(query) {
+      return query;
+    };
+
+    factory.selectResult = function(search_result) {
+      factory.buildSearch(search_result);
+    };
+
+    factory.buildSearch = function(search_options) {
+      console.log('building search object');
+      return new Search(search_options);
+    };
+
+    var location = function(args) {
+      this.name = args.name;
+      this.airport_code = args.airport_code;
+      this.latitude = args.latitude;
+      this.longitude = args.longitude;
+    };
+
+    // how to make this accept a object containing any of these parameters?
+    var Search = function(depart_location) {
+      this.depart_location = depart_location;
+      this.arrival_location = null;
+      this.depart_date = null;
+      this.return_date = null;
+    };
+
+    var searchResult = function(args) {
+      this.name = args.description;
+      this.airport_code = args.airport_code;
+      this.latitude = args.latitude;
+      this.longitude = args.longitude;
+    };
+
+    return factory;
+  }
+);
 
 
-
-
-
-
-
-var searchResult = function(args) {
-  this.description = args.description;
-}
