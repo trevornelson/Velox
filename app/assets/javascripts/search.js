740384e4a1eb55ea50f11ba98857b6619bc56867
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-27 21:30:59 -0400

    Created hydrateResults function

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,10 +3,33 @@ var myAppModule = angular.module('hither', []);
 myAppModule.controller('SearchController', function($scope) {
   // $scope.departing = new google.maps.places.AutocompleteService();
   // $scope.returning = new google.maps.places.AutocompleteService();
-  $scope.results = [];
+
+
 
   // show results ul when results.length > 0 && focused on element
-  $scope.autoComplete = function(service) {
+  $scope.autoComplete = function(event) {
+    console.log(event.target);
+    var search_box = event.target;
+    var placesService = new google.maps.places.AutocompleteService();
+
+    $(search_box).on('keyup', function(){
+      $scope.results = [];
+      var query = search_box.value;
+      console.log(query);
+      placesService.getQueryPredictions({ input: query }, callback);
+    });
+
+    function callback(predictions, status) {
+      if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
+      }
+
+      console.log(predictions);
+
+      $scope.hydrateResults(predictions);
+    }
+
     // find object that was focused on
     // create new autocomplete service object
     // add keypress listener on that object
@@ -14,8 +37,19 @@ myAppModule.controller('SearchController', function($scope) {
     //
   }
 
+  $scope.hydrateResults = function(search_results) {
+    search_results.forEach(function(el, ind, arr){
+      console.log("hydrating result");
+      $scope.results.push(new searchResult(el));
+    })
+  }
+
   // on result click...
   $scope.selectResult = function(places_object) {
     // hydrate Search object
   }
 });
+
+var searchResult = function(args) {
+  this.description = args.description;
+}
