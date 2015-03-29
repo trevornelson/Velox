487f7956fbd98ef88e1cd58a2123590d8b053b8b
Leo Rogers <leo.a.rogers@gmail.com>
2015-03-28 20:48:23 -0400

    cleaned up code in hotel controller by adding factory

--- app/assets/javascripts/hotels_filter.js
+++ app/assets/javascripts/hotels_filter.js
@@ -13,17 +13,22 @@ var Hotel = function(data) {
 };
 
   //create hotel controller
-    myAppModule.controller('HotelController', ['$scope', '$http', function($scope, $http) {
+  angular.module('hither').controller('HotelController', ['$scope', 'HotelFactory', function($scope, HotelFactory) {
+    HotelFactory.fetchHotels().success(function(data) {
+        $scope.hotels =  data.result.map(function(hotel_data) {
+        return new Hotel(hotel_data);
+      });
+     });
+  }] );
 
+  // create hotel factory
+  angular.module('hither').factory('HotelFactory', ['$http', function($http) {
+    var factory = {};
     var httpConfig = {  headers:{ "X-Mashape-Authorization": "N7SrCXP14imshrRVT7zdeMHz9NeLp1va6vFjsnpDJD7Fi1jnFg"}};
 
-    $scope.fetchHotels = function() {
-      $http.get('https://zilyo.p.mashape.com/search?latitude=52.5306438&longitude=13.3830683&pricemax=200&provider=airbnb', httpConfig)
-      .success(function(data) {
-       $scope.hotels = data.result.map(function(hotel_data) {
-        return new Hotel(hotel_data)
-      });
-     })
+    factory.fetchHotels = function() {
+      return $http.get('https://zilyo.p.mashape.com/search?latitude=52.5306438&longitude=13.3830683&pricemax=200&provider=airbnb', httpConfig);
     };
 
+    return factory;
   }] );
