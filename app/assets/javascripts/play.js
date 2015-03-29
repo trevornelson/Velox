71275b99753ad62eb96bd2e2a511492dd587c61c
Leo Rogers <leo.a.rogers@gmail.com>
2015-03-28 20:45:25 -0400

    cleaned up logic in hotel controller using factory

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -15,21 +15,35 @@ var Hotel = function(data) {
   //create hither module
   angular.module('hither', []);
 
-  // create hotel controller
-  angular.module('hither').controller('HotelController', ['$scope', '$http', function($scope, $http) {
+  //create hotel controller
+  angular.module('hither').controller('HotelController', ['$scope', 'HotelFactory', function($scope, HotelFactory) {
+
+    HotelFactory.fetchHotels().success(function(data) {
+        $scope.hotels =  data.result.map(function(hotel_data) {
+        return new Hotel(hotel_data);
+      });
+     });
+
+  }] );
+
+  // create hotel factory
+  angular.module('hither').factory('HotelFactory', ['$http', function($http) {
+
+    var factory = {};
 
     var httpConfig = {  headers:{ "X-Mashape-Authorization": "N7SrCXP14imshrRVT7zdeMHz9NeLp1va6vFjsnpDJD7Fi1jnFg"}};
 
-    $scope.fetchHotels = function() {
-      $http.get('https://zilyo.p.mashape.com/search?latitude=52.5306438&longitude=13.3830683&pricemax=200&provider=airbnb', httpConfig)
-      .success(function(data) {
-       $scope.hotels = data.result.map(function(hotel_data) {
-        return new Hotel(hotel_data)
-      });
-     })
+    factory.fetchHotels = function() {
+      return $http.get('https://zilyo.p.mashape.com/search?latitude=52.5306438&longitude=13.3830683&pricemax=200&provider=airbnb', httpConfig);
     };
 
+    return factory;
+
   }] );
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------
 
   //Given API output (array of data objects), return array of hydrated hotel objects
 // var createHotels = function(data) {
@@ -50,3 +64,21 @@ var Hotel = function(data) {
 //    return data.result.map(function(hotel_data) {return new Hotel(hotel_data)})
 //   });
 // };
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+  // create hotel controller
+  // angular.module('hither').controller('HotelController', ['$scope', '$http', function($scope, $http) {
+
+  //   var httpConfig = {  headers:{ "X-Mashape-Authorization": "N7SrCXP14imshrRVT7zdeMHz9NeLp1va6vFjsnpDJD7Fi1jnFg"}};
+
+  //   $scope.fetchHotels = function() {
+  //     $http.get('https://zilyo.p.mashape.com/search?latitude=52.5306438&longitude=13.3830683&pricemax=200&provider=airbnb', httpConfig)
+  //     .success(function(data) {
+  //      $scope.hotels = data.result.map(function(hotel_data) {
+  //       return new Hotel(hotel_data)
+  //     });
+  //    })
+  //   };
+
+  // }] );
