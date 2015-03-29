ac336c1d95306bb1d5072b08c6a367f71c1a205a
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-29 18:48:26 -0400

    Created a initAutocomplete function for initializing both autocomplete fields

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,56 +3,48 @@ var myAppModule = angular.module('hither', []);
 angular.module('hither')
   .controller('SearchController', ['$scope', 'searchFactory',
     function($scope, searchFactory) {
-
       $scope.search = new searchFactory.buildSearch();
 
       $scope.submit = function() {
         $scope.search.depart_date = new Date($scope.search.depart_date);
         $scope.search.return_date = new Date($scope.search.return_date);
-      }
+        console.log($scope.search);
+      };
 
-      var options = {types: ['(cities)'],
-                      componentRestrictions: {country: "us"}
-                    };
+      $scope.initAutocomplete = function(autocomplete_element, location_type) {
+        var options = {types: ['(cities)'],
+                        componentRestrictions: {country: "us"}
+                      };
+
+        google.maps.event.addListener(autocomplete_element, 'place_changed', function() {
+          var place = autocomplete_element.getPlace();
+          var lat = place.geometry.location.lat();
+          var lng = place.geometry.location.lng();
+
+          $scope.search[location_type] = {
+                                          latitude: lat,
+                                          longitude: lng,
+                                          name: place.formatted_address
+                                          };
+
+          searchFactory.fetchAirport(lat, lng)
+          .success(function(data, status, headers, config) {
+            $scope.search[location_type].airport_code = data.airports[0].code;
+          })
+          .error(function(data, status, headers, config) {
+            alert(status);
+          });
+        });
+      };
+
       var depart_input = document.getElementById('depart-autocomplete');
       var arrive_input = document.getElementById('destination-autocomplete');
-      $scope.depart_ac = new google.maps.places.Autocomplete(depart_input, options);
-      $scope.arrive_ac = new google.maps.places.Autocomplete(arrive_input, options);
+      $scope.depart_ac = new google.maps.places.Autocomplete(depart_input);
+      $scope.arrive_ac = new google.maps.places.Autocomplete(arrive_input);
 
+      $scope.initAutocomplete($scope.depart_ac, 'depart_location');
+      $scope.initAutocomplete($scope.arrive_ac, 'arrival_location');
 
-
-      google.maps.event.addListener($scope.depart_ac, 'place_changed', function() {
-        var place = $scope.depart_ac.getPlace();
-        $scope.search.depart_location.latitude = place.geometry.location.lat();
-        $scope.search.depart_location.longitude = place.geometry.location.lng();
-        $scope.search.depart_location.name = place.formatted_address;
-        searchFactory.fetchAirport(
-                                    $scope.search.depart_location.latitude,
-                                    $scope.search.depart_location.longitude)
-        .success(function(data, status, headers, config) {
-          $scope.search.depart_location.airport_code = data.airports[0].code;
-        })
-        .error(function(data, status, headers, config) {
-          alert(status);
-        });
-      });
-
-      google.maps.event.addListener($scope.arrive_ac, 'place_changed', function() {
-        var place = $scope.arrive_ac.getPlace();
-        $scope.search.arrival_location.latitude = place.geometry.location.lat();
-        $scope.search.arrival_location.longitude = place.geometry.location.lng();
-        $scope.search.arrival_location.name = place.formatted_address;
-        searchFactory.fetchAirport(
-                                    $scope.search.arrival_location.latitude,
-                                    $scope.search.arrival_location.longitude)
-        .success(function(data, status, headers, config) {
-          $scope.search.arrival_location.airport_code = data.airports[0].code;
-          console.log($scope.search);
-        })
-        .error(function(data, status, headers, config) {
-          alert(status);
-        });
-      });
     }
   ]);
 
