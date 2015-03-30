9e3c25110d1eb8c82f76953bb44537af6e83a44c
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-29 21:59:40 -0400

    Removed index action from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,14 @@
 class SessionsController < ApplicationController
 
-  def new
-    redirect_to curr_user_path if session[:user_id]
-  end
 
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to root_url
     else
       session[:message] = "invalid username and/or password"
-      redirect_to new_session_path
     end
+    redirect root
   end
 
   def destroy
