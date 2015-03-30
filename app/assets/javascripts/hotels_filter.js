74f755efb2136dd4c7c5283f7c5b3edf84f2495e
Christopher Selden <cbpselden@gmail.com>
2015-03-30 11:45:37 -0400

    both apis on page

--- app/assets/javascripts/hotels_filter.js
+++ app/assets/javascripts/hotels_filter.js
@@ -32,7 +32,7 @@ var Hotel = function(data) {
     var httpConfig = {  headers:{ "X-Mashape-Authorization": "N7SrCXP14imshrRVT7zdeMHz9NeLp1va6vFjsnpDJD7Fi1jnFg"}};
     factory.fetchHotels = function(lat, lng) {
       console.log("runs this only once before search is filled");
-      return $http.get('https://zilyo.p.mashape.com/search?latitude='+ lat + '&longitude=' + lng, httpConfig);
+      return $http.get('https://zilyo.p.mashape.com/search?latitude='+ lat + '&longitude=' + lng + '&maxdistance=35', httpConfig);
     };
 
     return factory;
