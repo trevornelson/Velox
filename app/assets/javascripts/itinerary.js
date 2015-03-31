6afc737cb585beae3c80cd8bc1e0ce7b76d9fbb7
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-31 10:30:05 -0400

    Removed itinerary factory

--- app/assets/javascripts/itinerary.js
+++ app/assets/javascripts/itinerary.js
@@ -3,8 +3,21 @@ myAppModule.controller('SaveItineraryController', ['$scope', '$rootScope', '$htt
 
 
       $scope.create = function() {
-        var json_itinerary = ItineraryFactory.parse($rootScope.hotels[$rootScope.hotels_index])
-        $http.post('/users/' + $rootScope.user.id + '/itineraries', json_itinerary)
+        var user_id = $rootScope.user.id
+
+        if (user_id) {
+          var json_itinerary = {
+                                user_id: user_id,
+                                hotels: $rootScope.hotels[$rootScope.hotels_index],
+                                trips: [$rootScope.trips[$rootScope.trips_index]]
+                                );
+
+          $http.post('/users/' + $rootScope.user.id + '/itineraries', json_itinerary).success(function(data) {
+            console.log(data);  // Redirect view in angular to user account page
+          });
+        } else {
+          $('#login-modal').modal();
+        }
       };
 
       $scope.destroy = function() {
@@ -12,14 +25,3 @@ myAppModule.controller('SaveItineraryController', ['$scope', '$rootScope', '$htt
       }
     }
   ]);
-
-myAppModule.factory('ItineraryFactory', function() {
-  var factory = {};
-
-  factory.parse = function(){
-    // parse itinerary into correct format for rails
-  }
-
-
-  return factory;
-});
