40328d9c9cb3ef4c3323e30c712851e561a075d5
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-30 23:52:31 -0400

    Added itineraries controller

---
+++ app/assets/javascripts/itinerary.js
@@ -0,0 +1,18 @@
+myAppModule.controller('SaveItineraryController', ['$scope', '$rootScope',
+    function($scope, $rootScope) {
+
+
+      $scope.create = function() {
+
+      };
+    }
+  ]);
+
+myAppModule.factory('ItineraryFactory', function() {
+  var factory = {};
+
+  factory.parse
+
+
+  return factory;
+});
