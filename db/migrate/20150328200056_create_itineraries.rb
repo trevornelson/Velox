4b0a9fd01af92f572bcc8cb7e060618d18c3504e
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-30 23:05:22 -0400

    Added title column to itineraries migration

--- db/migrate/20150328200056_create_itineraries.rb
+++ db/migrate/20150328200056_create_itineraries.rb
@@ -1,7 +1,8 @@
 class CreateItineraries < ActiveRecord::Migration
   def change
     create_table :itineraries do |t|
-      t.references :user
+      t.references  :user
+      t.string      :title
 
       t.timestamps null: false
     end
