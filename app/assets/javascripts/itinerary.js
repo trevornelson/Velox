e7ba6478d77ea03ef13f34917a8eb535dc1b0747
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-31 10:33:55 -0400

    Fixed type in ItineraryController create function

--- app/assets/javascripts/itinerary.js
+++ app/assets/javascripts/itinerary.js
@@ -1,4 +1,4 @@
-myAppModule.controller('SaveItineraryController', ['$scope', '$rootScope', '$http', 'ItineraryFactory',
+myAppModule.controller('ItineraryController', ['$scope', '$rootScope', '$http',
     function($scope, $rootScope, $http, ItineraryFactory) {
 
 
@@ -10,7 +10,7 @@ myAppModule.controller('SaveItineraryController', ['$scope', '$rootScope', '$htt
                                 user_id: user_id,
                                 hotels: $rootScope.hotels[$rootScope.hotels_index],
                                 trips: [$rootScope.trips[$rootScope.trips_index]]
-                                );
+                                };
 
           $http.post('/users/' + $rootScope.user.id + '/itineraries', json_itinerary).success(function(data) {
             console.log(data);  // Redirect view in angular to user account page
