302e2f16a2e4d496ecb2d9497c8b8cbdb91d105f
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-31 18:15:37 -0400

    Updated flight times for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 user = User.create(username: 'demo_user', password: '12345', email: 'demo@example.com')
+user2 = User.create(username: 'demo_user2', password: '67890', email: 'demo2@example.com')
 
 hotel_imgs = ['http://www.mayfairhotelandspa.com/_images/_design/headers/rooftop2.jpg', 'https://www.gametimesc.com/Content/images/rooms.jpg', 'http://airporthotelguru.com/wp-content/uploads/2008/03/yvrfair1.jpg', 'http://hotelsistanbul.us/wp-content/uploads/2014/07/Hotel-Software-Cloud-Based-Hotel-and-Digital-Marketing.jpg']
 
@@ -11,19 +12,19 @@ hotel_imgs = ['http://www.mayfairhotelandspa.com/_images/_design/headers/rooftop
                                               'flight_num' => Faker::Code.isbn,
                                               'carrier_full' => 'United',
                                               'airport_ori_code' => 'LGA',
-                                              'airport_dest_code' => 'SFO',
-                                              'duration' => '4 hours, 29 min',
-                                              'dep_time' => Faker::Time.between(1.days.ago, Time.now, :morning),
-                                              'arr_time' => Faker::Time.between(1.days.ago, Time.now, :evening)
+                                              'airport_dest_code' => 'DFW',
+                                              'duration' => '3 hours, 10 min',
+                                              'dep_time' => '8:47 AM',
+                                              'arr_time' => '9:57 AM')
                                               },
                                               {'carrier_abbv' => 'DTA',
                                                 'flight_num' => Faker::Code.isbn,
                                                 'carrier_full' => 'Delta',
-                                                'airport_ori_code' => 'EWR',
-                                                'airport_dest_code' => 'DFW',
-                                                'duration' => '3 hours, 10 min'},
-                                                'dep_time' => Faker::Time.between(1.days.ago, Time.now, :morning),
-                                                'arr_time' => Faker::Time.between(1.days.ago, Time.now, :evening)]
+                                                'airport_ori_code' => 'DFW',
+                                                'airport_dest_code' => 'SFO',
+                                                'duration' => '4 hours, 29 min',
+                                                'dep_time' => '11:34 AM',
+                                                'arr_time' => '3:03 PM'}]
                                               }],
                     'hotel' => {'name' => Faker::Company.name,
                               'city' => Faker::Address.city,
@@ -37,4 +38,5 @@ hotel_imgs = ['http://www.mayfairhotelandspa.com/_images/_design/headers/rooftop
                   }
 
   user.itineraries.create_with_all_relations(json_itinerary)
+  user2.itineraries.create_with_all_relations(json_itinerary)
 end
