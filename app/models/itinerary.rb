0058326ce3ba076f1d11ad5aca6c0cb9624bc9bc
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-30 22:13:27 -0400

    Added initial create_with_all_relations Itinerary method

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -8,4 +8,32 @@ class Itinerary < ActiveRecord::Base
                                                       :hotel
                                                       ).find(user_id)}
 
+  def self.create_with_all_relations(params)
+    itinerary = self.new(params[:itinerary_title], params[:user_id])
+    success? = true
+    params[:trips].each do |trip|
+      t = itinerary.trips.new(trip[:details])
+      if valid?
+        t.save
+        t.flights.each do |flight|
+          f = t.flights.new(flight[:details])
+          if f.valid?
+            f.save
+          else
+            success? = false
+          end
+        end
+      else
+        success? = false
+      end
+    end
+    h = itinerary.hotel.new(params[:hotel])
+    if h.valid?
+      h.save
+    else
+      success? = false
+    end
+    return success?
+  end
+
 end
