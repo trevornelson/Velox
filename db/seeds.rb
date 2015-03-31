15e1f064a6216c24436bc0cad595042d2b6f7208
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-30 23:32:19 -0400

    Added loop to itineraries seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,32 @@ user = User.create(username: 'demo_user', password: '12345', email: 'demo@exampl
 
 hotel_imgs = ['http://www.mayfairhotelandspa.com/_images/_design/headers/rooftop2.jpg', 'https://www.gametimesc.com/Content/images/rooms.jpg', 'http://airporthotelguru.com/wp-content/uploads/2008/03/yvrfair1.jpg', 'http://hotelsistanbul.us/wp-content/uploads/2014/07/Hotel-Software-Cloud-Based-Hotel-and-Digital-Marketing.jpg']
 
-json_itinerary = {'itinerary_title': Faker::Lorem.words,
-                  'user_id': user.id,
-                  'trips': [{'flights': {'carrier_abbv': 'UNT',
-                                          'airport_ori_code': 'LGA',
-                                          'airport_dest_code': 'SFO',
-                                          'duration': '4 hours, 29 min'
+json_itinerary = {'itinerary_title' => Faker::Lorem.sentence,
+                  'user_id' => 1,
+                  'trips' => [{'price' => '476.32',
+                              'duration' => '7 hours, 39 min',
+                              'flights' => [{'carrier_abbv' => 'UNT',
+                                          'airport_ori_code' => 'LGA',
+                                          'airport_dest_code' => 'SFO',
+                                          'duration' => '4 hours, 29 min'
                                         },
-                                        {'carrier_abbv': 'DTA',
-                                          'airport_ori_code': 'EWR',
-                                          'airport_dest_code': 'DFW',
-                                          'duration': '3 hours, 10 min'}
-                            }],
-                  'hotel': {'name': Faker::Company.name,
-                            'city': Faker::Address.city,
-                            'neighborhood': Faker::Lorem.word,
-                            'postal_code': Faker::Address.zip_code,
-                            'street_name': Faker::Address.street_name,
-                            'price': Faker::Commerce.price,
-                            'provider': Faker::Company.name,
-                            'photo_url': hotel_imgs.sample,
-                            'url': Faker::Internet.domain_name}
+                                        {'carrier_abbv' => 'DTA',
+                                          'airport_ori_code' => 'EWR',
+                                          'airport_dest_code' => 'DFW',
+                                          'duration' => '3 hours, 10 min'}]
+                                        }],
+                  'hotel' => {'name' => Faker::Company.name,
+                            'city' => Faker::Address.city,
+                            'neighbourhood' => Faker::Lorem.word,
+                            'postal_code' => Faker::Address.zip_code,
+                            'street_name' => Faker::Address.street_name,
+                            'price' => '326.76',
+                            'provider' => Faker::Company.name,
+                            'photo_url' => hotel_imgs.sample,
+                            'url' => Faker::Internet.domain_name}
                 }
 
 
-user.itineraries.create_with_all_relations(json_itinerary)
+4.times do
+  user.itineraries.create_with_all_relations(json_itinerary)
+end
