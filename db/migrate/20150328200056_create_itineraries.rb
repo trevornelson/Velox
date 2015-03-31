78f5e8c0a7b9442f95dadffdf2f6de0758ffc0fe
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-31 10:16:42 -0400

    Removed title column from itineraries migration

--- db/migrate/20150328200056_create_itineraries.rb
+++ db/migrate/20150328200056_create_itineraries.rb
@@ -2,7 +2,6 @@ class CreateItineraries < ActiveRecord::Migration
   def change
     create_table :itineraries do |t|
       t.references  :user
-      t.string      :title
 
       t.timestamps null: false
     end
