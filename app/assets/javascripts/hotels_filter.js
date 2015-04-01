6e8c152e73486249e85c3992ae42fc885bd935d8
Christopher Selden <cbpselden@gmail.com>
2015-04-01 00:05:53 -0400

    added awesome toggle buttons

--- app/assets/javascripts/hotels_filter.js
+++ app/assets/javascripts/hotels_filter.js
@@ -63,12 +63,7 @@ var Hotel = function(data) {
     var factory = {};
     var httpConfig = {  headers:{ "X-Mashape-Authorization": "N7SrCXP14imshrRVT7zdeMHz9NeLp1va6vFjsnpDJD7Fi1jnFg"}};
     factory.fetchHotels = function(lat, lng) {
-<<<<<<< HEAD
-      return $http.get('https://zilyo.p.mashape.com/search?latitude='+ lat + '&longitude=' + lng + '&maxdistance=35', httpConfig);
-=======
-      console.log("runs this only once before search is filled");
       return $http.get('https://zilyo.p.mashape.com/search?latitude='+ lat + '&longitude=' + lng + '&maxdistance=100', httpConfig);
->>>>>>> added widget-esque box for flights
     };
 
     return factory;
