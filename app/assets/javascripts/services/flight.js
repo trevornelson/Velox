72a6cd87edd59eaf4e267da10bee5a52486de383
trevornelson <trevor.nelson1@gmail.com>
2015-05-07 17:16:57 -0400

    Broadcast trips.update from FlightSvc when trips collection is updated

--- app/assets/javascripts/services/flight.js
+++ app/assets/javascripts/services/flight.js
@@ -1,30 +1,34 @@
 /**
 Service for querying Google QPX Api and storing responses
 **/
-App.service('FlightSvc', ['$http',
-                  function($http) {
+App.service('FlightSvc', ['$rootScope', '$http',
+                  function($rootScope,   $http) {
 
   var service = {
     trips: [],
     qpxKey: 'AIzaSyCN4pjLu5qK0ylXiV3wlEkrBXUKCz6BuLE',
+
+    // Returns json template for generating QPX API post query data
     getRequestTemplate: function() {
       var reqTemplate = {
-      "request": {
-        "slice": [],
-        "passengers": {
-          "adultCount": 1,
-          "infantInLapCount": 0,
-          "infantInSeatCount": 0,
-          "childCount": 0,
-          "seniorCount": 0
-        },
-        "solutions": 50,
-        "refundable": false
-      }
-    };
+        "request": {
+          "slice": [],
+          "passengers": {
+            "adultCount": 1,
+            "infantInLapCount": 0,
+            "infantInSeatCount": 0,
+            "childCount": 0,
+            "seniorCount": 0
+          },
+          "solutions": 50,
+          "refundable": false
+        }
+      };
 
-    return reqTemplate;
+      return reqTemplate;
     },
+
+    // push trip leg into template's 'slice' array to make implementing two-way trips easier later.
     createRequest: function(opts) {
       var req = getRequestTemplate();
       var trip_leg = {
@@ -33,17 +37,25 @@ App.service('FlightSvc', ['$http',
                       "date": opts.depart_date
                       }
 
-      // push trip leg into slice array to make implementing two-way trips easier later
       req.request.slice.push(trip_leg);
 
       return JSON.stringify(req);
     },
+
+    // queries QPX API, returns a promise and resolves with JSON query response
     fetch: function(opts) {
-      // queries QPX API, returns a promise until query is complete.
       var data = service.createRequest(opts);
       var url = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=' + service.qpxKey;
 
       return $http.post(url, data);
     }
+
+    // Once all queries are done, set trips collection and broadcast trips.update event.
+    updateTrips: function(trips) {
+      service.trips = trips;
+      $rootScope.$broadcast('trips.update');
+    }
   }
+
+  return service;
 }]);
