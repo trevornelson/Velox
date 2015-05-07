2f5f153033b54f366ee5d778f8e3f3c46d528536
trevornelson <trevor.nelson1@gmail.com>
2015-05-07 16:47:46 -0400

    Add createRequest and fetch functions for HotelSvc service

--- app/assets/javascripts/services/hotel.js
+++ app/assets/javascripts/services/hotel.js
@@ -2,10 +2,22 @@
 Service for querying Zilyo API and storing a collection of accommedations
 **/
 App.service('HotelSvc', ['$http',
-                  function($http) {
+                function( $http ) {
 
   var service = {
-    hotels: [];
+    hotels: [],
+    zilyoAuth: {
+      headers: { "X-Mashape-Authorization": "S4ftN0SMuLmsh8gvTCpVF3OfIcRKp1CTJqMjsnUJuNbU826Uz4"}
+    },
+    createRequest: function(destination) {
+      'https://zilyo.p.mashape.com/search?latitude='+ destination.lat + '&longitude=' + destination.lng + '&maxdistance=100'
+
+    },
+    fetch: function(destination) {
+      var url = service.createRequest(destination);
+
+      return $http.get(url, service.zilyoAuth);
+    } 
   }
 
   return service;
