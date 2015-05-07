f8a25c00b8d820f912057278aa638a0913fc7c80
trevornelson <trevor.nelson1@gmail.com>
2015-05-07 15:49:51 -0400

    Create ResultsController for result filtering

--- app/assets/javascripts/controllers/search.js
+++ app/assets/javascripts/controllers/search.js
@@ -1,5 +1,9 @@
+/**
+Controller for trip search input fields
+**/
 App.controller('SearchController', ['$scope', 'SearchSvc', 'FlightSvc', 'HotelSvc',
-                            function($scope, SearchSvc, HotelSvc) {
+                            function($scope,   SearchSvc,   FlightSvc,   HotelSvc) {
+  
   $scope.$on('search.update', function(event) {
     $scope.trips = SearchSvc.trips;
     $scope.hotels = SearchSvc.hotels;
@@ -7,5 +11,17 @@ App.controller('SearchController', ['$scope', 'SearchSvc', 'FlightSvc', 'HotelSv
 
   $scope.trips = [];
   $scope.hotels = [];
+
+}]);
+
+/**
+Controller for itinerary results filters
+**/
+App.controller('ResultsController', ['$scope', 'FlightSvc', 'HotelSvc',
+                            function($scope,   FlightSvc,   HotelSvc) {
+
+
+
+
 
 }]);
