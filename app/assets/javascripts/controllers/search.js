44a33df52a1d212bd01f9c68603020b743ff6109
trevornelson <trevor.nelson1@gmail.com>
2015-05-07 16:18:50 -0400

    Move ResultsController to separate file

--- app/assets/javascripts/controllers/search.js
+++ app/assets/javascripts/controllers/search.js
@@ -7,21 +7,3 @@ App.controller('SearchController', ['$scope', 'SearchSvc',
 
 
 }]);
-
-/**
-Controller for itinerary results filters
-**/
-App.controller('ResultsController', ['$scope', 'FlightSvc', 'HotelSvc',
-                            function($scope,   FlightSvc,   HotelSvc) {
-
-  $scope.$on('trips.update', function(event) {
-    $scope.trips = FlightSvc.trips;
-  });
-
-  $scope.$on('hotels.update', function(event) {
-    $scope.hotels = HotelSvc.hotels;
-  });
-
-
-
-}]);
