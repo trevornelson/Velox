6a32e124127f73dd0f680e89f6579b6ca61e800a
trevornelson <trevor.nelson1@gmail.com>
2015-05-07 17:34:29 -0400

    Create UserSvc service

---
+++ app/assets/javascripts/services/user.js
@@ -0,0 +1,13 @@
+/**
+Service for user authentication and querying app backend for user data.
+**/
+App.service('UserSvc', ['$rootScope',
+                function($rootScope) {
+
+  var service = {
+
+
+  };
+
+  return service;
+}]);
