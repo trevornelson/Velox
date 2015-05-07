6984b63a0f32a5ed26a9a17e98fe371f545bd2a8
trevornelson <trevor.nelson1@gmail.com>
2015-05-07 11:25:33 -0400

    Assign initial search variables to SearchSvc

---
+++ app/assets/javascripts/services/search.js
@@ -0,0 +1,8 @@
+myAppModule.service('SearchSvc', ['$rootScope', function($rootScope) {
+  var service = {
+    arrival_location: '',
+    depart_location: '',
+    depart_date: '',
+    return_date: '',
+  }
+}]);
