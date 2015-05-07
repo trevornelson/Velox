db015c6c1cb3d7f2f184a7b21307cd600afc79e6
trevornelson <trevor.nelson1@gmail.com>
2015-05-07 17:07:01 -0400

    Broadcast hotels.update from HotelSvc when collection is updated

--- app/assets/javascripts/services/hotel.js
+++ app/assets/javascripts/services/hotel.js
@@ -1,23 +1,31 @@
 /**
 Service for querying Zilyo API and storing a collection of accommedations
 **/
-App.service('HotelSvc', ['$http',
-                function( $http ) {
+App.service('HotelSvc', ['$http', '$rootScope',
+                function( $http,   $rootScope ) {
 
   var service = {
     hotels: [],
+
     zilyoAuth: {
       headers: { "X-Mashape-Authorization": "S4ftN0SMuLmsh8gvTCpVF3OfIcRKp1CTJqMjsnUJuNbU826Uz4"}
     },
+    
     createRequest: function(destination) {
-      'https://zilyo.p.mashape.com/search?latitude='+ destination.lat + '&longitude=' + destination.lng + '&maxdistance=100'
-
+      return 'https://zilyo.p.mashape.com/search?latitude='+ destination.lat + '&longitude=' + destination.lng + '&maxdistance=100';
     },
+
     fetch: function(destination) {
       var url = service.createRequest(destination);
 
       return $http.get(url, service.zilyoAuth);
-    } 
+    },
+
+    // Once all queries are done, set hotels collection and broadcast hotels.update event.
+    updateHotels: function(hotels) {
+      service.hotels = hotels;
+      $rootScope.$broadcast('hotels.update');
+    }
   }
 
   return service;
