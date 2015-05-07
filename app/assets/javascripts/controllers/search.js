ab97d2f383aa87d2c36c622a667d1dc9b4600a83
trevornelson <trevor.nelson1@gmail.com>
2015-05-07 16:03:05 -0400

    Move flights and hotels collections to ResultsController

--- app/assets/javascripts/controllers/search.js
+++ app/assets/javascripts/controllers/search.js
@@ -1,16 +1,10 @@
 /**
 Controller for trip search input fields
 **/
-App.controller('SearchController', ['$scope', 'SearchSvc', 'FlightSvc', 'HotelSvc',
-                            function($scope,   SearchSvc,   FlightSvc,   HotelSvc) {
+App.controller('SearchController', ['$scope', 'SearchSvc',
+                            function($scope,   SearchSvc) {
   
-  $scope.$on('search.update', function(event) {
-    $scope.trips = SearchSvc.trips;
-    $scope.hotels = SearchSvc.hotels;
-  });
 
-  $scope.trips = [];
-  $scope.hotels = [];
 
 }]);
 
@@ -20,7 +14,13 @@ Controller for itinerary results filters
 App.controller('ResultsController', ['$scope', 'FlightSvc', 'HotelSvc',
                             function($scope,   FlightSvc,   HotelSvc) {
 
+  $scope.$on('trips.update', function(event) {
+    $scope.trips = FlightSvc.trips;
+  });
 
+  $scope.$on('hotels.update', function(event) {
+    $scope.hotels = HotelSvc.hotels;
+  });
 
 
 
