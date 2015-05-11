b6cc742aec0c7de7b9b944395f5952fc81601d38
trevornelson <trevor.nelson1@gmail.com>
2015-05-11 15:48:10 -0400

    Update queryAPIs function to return a deferred promise

--- app/assets/javascripts/services/search.js
+++ app/assets/javascripts/services/search.js
@@ -15,13 +15,18 @@ App.service('SearchSvc', ['$rootScope', '$q', 'FlightSvc', 'HotelSvc',
 
     // Query 3rd party APIs and update corresponding collections once they've resolved.
     queryAPIs: function() {
+      var deferred = $q.defer();
+
       var flightsPromise = FlightSvc.fetch(service.searchOptions);
       var hotelsPromise = HotelSvc.fetch(service.searchOptions);
 
       $q.all([flightsPromise, hotelsPromise]).then(function(data){
         FlightSvc.updateTrips(data[0]);
         HotelSvc.updateHotels(data[1]);
+        deferred.resolve(true);
       });
+
+      return deferred.promise;
     }
   }
 
