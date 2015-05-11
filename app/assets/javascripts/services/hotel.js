9a4e1c7338c051bf887da8d4f5951215691408c8
trevornelson <trevor.nelson1@gmail.com>
2015-05-11 17:39:20 -0400

    Create parseZilyo function for HotelSvc service

--- app/assets/javascripts/services/hotel.js
+++ app/assets/javascripts/services/hotel.js
@@ -21,6 +21,28 @@ App.service('HotelSvc', ['$rootScope', '$http',
       return $http.get(url, service.zilyoAuth);
     },
 
+    parseZilyo: function(response) {
+      function Hotel(data) {
+        this.name = data.attr.heading;
+        this.city = data.location.city;
+        this.neighbourhood = data.location.neighbourhood;
+        this.postal_code = data.location.postalCode;
+        this.street_name = data.location.streetName;
+        this.price = data.price.nightly;
+        this.provider = data.provider.full;
+        this.availability = data.availability;
+        this.photo_url = data.photos[0].large;
+        this.url = data.provider.url;
+      };
+
+
+      var hotels = response.result.map(function(hotel_data) {
+        return new Hotel(hotel_data);
+      });
+
+      service.updateHotels(hotels);
+    },
+
     // Once all queries are done, set hotels collection and broadcast hotels.update event.
     updateHotels: function(hotels) {
       service.hotels = hotels;
