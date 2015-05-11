c33bf09a0338f7dbff9a6447adf0d0d8ce9ebaab
trevornelson <trevor.nelson1@gmail.com>
2015-05-11 19:59:43 -0400

    Add search and airports to SearchController scope

--- app/assets/javascripts/controllers/search.js
+++ app/assets/javascripts/controllers/search.js
@@ -4,6 +4,7 @@ Controller for trip search input fields
 App.controller('SearchController', ['$scope', 'SearchSvc',
                             function($scope,   SearchSvc) {
   
-
+  $scope.search = SearchSvc;
+  $scope.airports = autocomplete_source;
 
 }]);
