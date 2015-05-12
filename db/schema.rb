98a963352352c794057800ce46c070a26f20f0f5
trevornelson <trevor.nelson1@gmail.com>
2015-05-11 21:53:01 -0400

    Switch database to SQLite3 temporarily

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150328203504) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "flights", force: :cascade do |t|
     t.integer  "trip_id"
     t.string   "carrier_abbv",      null: false
